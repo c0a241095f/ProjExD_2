a005195faf8ffbcef11adf6c2f0facabc61ce68a
Sano <c0a241095f@edu.teu.ac.jp>
2025-11-25 14:30:46 +0900

    practice Q3

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -7,11 +7,25 @@ import pygame as pg
 WIDTH, HEIGHT = 1100, 650
 DELTA = {
    pg.K_UP:(0,-5),
-   pg.K_DOWN:(0,5),
+   pg.K_DOWN:(0,+5),
    pg.K_LEFT:(-5,0),
-   pg.K_RIGHT:(5,0),
+   pg.K_RIGHT:(+5,0),
 }
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
+
+def check_bound(obj_rct: pg.Rect) -> tuple[bool, bool]:
+    
+# 引数：こうかとんRectかばくだんRect
+# 戻り値：タプル（横方向判定結果，縦方向判定結果）
+# 画面内ならTrue，画面外ならFalse
+
+    yoko,tate=True,True
+    if obj_rct.left<0 or WIDTH <obj_rct.right:  #横方向のはみだしチェック
+        yoko=False
+    if obj_rct.top<0 or HEIGHT <obj_rct.bottom:  #縦方向のはみだしチェック
+        tate=False
+
+    return yoko ,tate
 
 
 def main():
@@ -55,6 +69,8 @@ def main():
         
         # bb_rct.move_ip(sum_mv)
         kk_rct.move_ip(sum_mv)
+        if check_bound(kk_rct)!=(True,True):
+            kk_rct.move_ip(-sum_mv[0],-sum_mv[1])
         screen.blit(kk_img, kk_rct)
         bb_rct.move_ip(vx,vy)
         screen.blit(bb_img, bb_rct)
